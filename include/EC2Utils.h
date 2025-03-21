5b94b3e137626abc5f9c5da82d92e96907473cb5
KreedM <blackknight3377@gmail.com>
2025-03-20 21:30:48 -0700

    License information in files

--- include/EC2Utils.h
+++ include/EC2Utils.h
@@ -1,3 +1,19 @@
+// This file is part of EC2-Instance-Manager
+// Copyright (C) 2025  Derek Meng
+
+// EC2-Instance-Manager is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// EC2-Instance-Manager is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with EC2-Instance-Manager.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef EC2UTILS_H
 #define EC2UTILS_H
 
